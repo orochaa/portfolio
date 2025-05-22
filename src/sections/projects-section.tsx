99acb924083ef2f98245ac66ab6b45a9fc131f6f
Bruno Rocha <brunorocha2674@gmail.com>
2025-05-21 21:52:37 -0300

    fix: fix projects section backgound for mobile and navigators

--- src/sections/projects-section.tsx
+++ src/sections/projects-section.tsx
@@ -97,84 +97,86 @@ export function ProjectsSection(): React.JSX.Element {
   }, [selectedFilters])
 
   return (
-    <div id="projects" className="mx-auto w-11/12 max-w-6xl py-20">
-      <h2 className="bg-background sticky top-[3.05rem] left-0 z-20 mb-6 pt-4 pb-3 text-2xl after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:bg-linear-to-r after:from-zinc-400 after:to-zinc-200">
-        {t('projects.title')}
-      </h2>
-      <div className="flex flex-col gap-4 sm:flex-row sm:gap-0">
-        <nav className="relative left-0 h-fit sm:sticky sm:top-32">
-          <p className="mb-1.5 text-base font-semibold">
-            {t('projects.categories')}
-          </p>
-          <div className="overflow-x-auto pb-3">
-            <ul className="flex w-fit flex-row gap-1 border-zinc-600 sm:w-36 sm:flex-col sm:border-l">
-              {headings.map(h => (
-                <li key={h.id}>
-                  <a
-                    href={`#${h.id}`}
-                    data-active={activeId === h.id}
-                    className="relative block rounded-r px-3 py-1.5 text-base before:absolute before:bottom-0 before:left-[-1px] before:h-full before:w-0 before:bg-zinc-600 hover:bg-zinc-900 hover:before:bg-zinc-500 active:bg-zinc-900 active:before:bg-zinc-500 data-active:bg-zinc-800 data-active:font-medium data-active:before:bg-blue-400 sm:before:w-[1px]"
-                    onClick={scrollTo(`#${h.id}`, 125)}
-                  >
-                    {h.text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <p className="mt-4 mb-1.5 text-base font-semibold">
-            {t('projects.filters')}
-          </p>
-          <div className="overflow-x-auto pb-3">
-            <ul className="flex w-fit flex-row gap-1 border-b border-zinc-600 sm:w-36 sm:flex-col sm:border-0 sm:border-l">
-              {filterOptions.map(option => {
-                const isSelected = selectedFilters.includes(option)
-
-                return (
-                  <li key={option}>
-                    <button
-                      type="button"
-                      data-selected={isSelected}
-                      className="relative block w-full rounded-r px-3 py-1.5 text-left text-base whitespace-nowrap before:absolute before:bottom-[-1px] before:left-0 before:h-[1px] before:w-full before:bg-zinc-600 hover:bg-zinc-900 hover:before:bg-zinc-500 active:bg-zinc-900 active:before:bg-zinc-500 data-selected:bg-zinc-800 data-selected:font-medium data-selected:before:bg-orange-400 sm:before:bottom-0 sm:before:left-[-1px] sm:before:h-full sm:before:w-[1px]"
-                      // eslint-disable-next-line react/jsx-no-bind
-                      onClick={() =>
-                        setSelectedFilters(state =>
-                          isSelected
-                            ? state.filter(o => o !== option)
-                            : [...state, option]
-                        )
-                      }
+    <div className="bg-background py-20">
+      <div id="projects" className="mx-auto w-11/12 max-w-6xl">
+        <h2 className="bg-background top-[3.05rem] left-0 z-20 mb-6 pt-4 pb-3 text-2xl after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:bg-linear-to-r after:from-zinc-400 after:to-zinc-200 sm:sticky">
+          {t('projects.title')}
+        </h2>
+        <div className="flex flex-col gap-4 sm:flex-row sm:gap-0">
+          <nav className="relative left-0 h-fit sm:sticky sm:top-32">
+            <p className="mb-1.5 text-base font-semibold">
+              {t('projects.categories')}
+            </p>
+            <div className="overflow-x-auto pb-3">
+              <ul className="flex w-fit flex-row gap-1 border-zinc-600 sm:w-36 sm:flex-col sm:border-l">
+                {headings.map(h => (
+                  <li key={h.id}>
+                    <a
+                      href={`#${h.id}`}
+                      data-active={activeId === h.id}
+                      className="relative block rounded-r px-3 py-1.5 text-base before:absolute before:bottom-0 before:left-[-1px] before:h-full before:w-0 before:bg-zinc-600 hover:bg-zinc-900 hover:before:bg-zinc-500 active:bg-zinc-900 active:before:bg-zinc-500 data-active:bg-zinc-800 data-active:font-medium data-active:before:bg-blue-400 sm:before:w-[1px]"
+                      onClick={scrollTo(`#${h.id}`, 125)}
                     >
-                      {option}
-                    </button>
+                      {h.text}
+                    </a>
                   </li>
-                )
-              })}
-            </ul>
+                ))}
+              </ul>
+            </div>
+            <p className="mt-4 mb-1.5 text-base font-semibold">
+              {t('projects.filters')}
+            </p>
+            <div className="overflow-x-auto pb-3">
+              <ul className="flex w-fit flex-row gap-1 border-b border-zinc-600 sm:w-36 sm:flex-col sm:border-0 sm:border-l">
+                {filterOptions.map(option => {
+                  const isSelected = selectedFilters.includes(option)
+
+                  return (
+                    <li key={option}>
+                      <button
+                        type="button"
+                        data-selected={isSelected}
+                        className="relative block w-full rounded-r px-3 py-1.5 text-left text-base whitespace-nowrap before:absolute before:bottom-[-1px] before:left-0 before:h-[1px] before:w-full before:bg-zinc-600 hover:bg-zinc-900 hover:before:bg-zinc-500 active:bg-zinc-900 active:before:bg-zinc-500 data-selected:bg-zinc-800 data-selected:font-medium data-selected:before:bg-orange-400 sm:before:bottom-0 sm:before:left-[-1px] sm:before:h-full sm:before:w-[1px]"
+                        // eslint-disable-next-line react/jsx-no-bind
+                        onClick={() =>
+                          setSelectedFilters(state =>
+                            isSelected
+                              ? state.filter(o => o !== option)
+                              : [...state, option]
+                          )
+                        }
+                      >
+                        {option}
+                      </button>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </nav>
+          <div className="grow space-y-8 sm:px-6">
+            {filteredGroups.length > 0 ? (
+              filteredGroups.map((group, groupIndex) => (
+                <div key={group.name}>
+                  <h3 id={group.name} className="mb-2 text-xl font-semibold">
+                    {group.name}
+                  </h3>
+                  <ul className="grid w-fit grid-cols-1 gap-2 xl:grid-cols-2">
+                    {group.projects.map((project, projectIndex) => (
+                      <Project
+                        key={project.name}
+                        project={project}
+                        groupIndex={groupIndex}
+                        projectIndex={projectIndex}
+                      />
+                    ))}
+                  </ul>
+                </div>
+              ))
+            ) : (
+              <p>{t('projects.notFound')}</p>
+            )}
           </div>
-        </nav>
-        <div className="grow space-y-8 sm:px-6">
-          {filteredGroups.length > 0 ? (
-            filteredGroups.map((group, groupIndex) => (
-              <div key={group.name}>
-                <h3 id={group.name} className="mb-2 text-xl font-semibold">
-                  {group.name}
-                </h3>
-                <ul className="grid w-fit grid-cols-1 gap-2 xl:grid-cols-2">
-                  {group.projects.map((project, projectIndex) => (
-                    <Project
-                      key={project.name}
-                      project={project}
-                      groupIndex={groupIndex}
-                      projectIndex={projectIndex}
-                    />
-                  ))}
-                </ul>
-              </div>
-            ))
-          ) : (
-            <p>{t('projects.notFound')}</p>
-          )}
         </div>
       </div>
     </div>
