4d7198d62ec661d7e50060823337b68bddc00f06
Bruno Rocha <brunorocha2674@gmail.com>
2025-06-09 12:00:34 -0300

    fix: title text size for mobile

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -8,7 +8,7 @@ export function Title(props: TitleProps): React.JSX.Element {
     <h2
       {...props}
       className={cn(
-        'relative mb-8 px-1 pb-3.5 text-4xl font-semibold',
+        'relative mb-8 px-1 pb-3.5 text-3xl font-semibold sm:text-4xl',
         'after:absolute after:bottom-0 after:left-0 after:h-1 after:w-full',
         'after:bg-gradient-to-r after:from-blue-500 after:to-cyan-500 after:drop-shadow-[0rem_-0.125rem_0.5rem] after:drop-shadow-sky-500',
         props.className
